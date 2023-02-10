10769e171c597f154be6f02e4d5024c106831c8d
Sundaram49 <86216867+Sundaram49@users.noreply.github.com>
2023-02-10 13:58:21 +0530

    file uploaded

---
+++ App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <> 
+     <div className="App">
+      <h1>Wether App</h1>
+    
+    <div className='container'>
+     <div className='search'>
+      <input type="search"  placeholder='Search' ></input>
+      <br></br>
+      <h1>Area</h1>
+       <h3>Temprature</h3>
+     </div>
+    </div>
+    </div>
+    </>
+  
+  );
+}
+
+export default App;
